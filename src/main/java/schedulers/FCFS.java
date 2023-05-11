20743996cbde9eb29744c9ad9c51e46f68db684f
chambored <nicholasatrimmer@gmail.com>
2023-05-11 14:19:13 -0700

    updates to algo classes and new metric generator

--- src/main/java/schedulers/FCFS.java
+++ src/main/java/schedulers/FCFS.java
@@ -2,13 +2,28 @@ package schedulers;
 
 import java.util.List;
 
+import utilities.PerformanceMetricGenerator;
+import utilities.Process;
+
+
 /**
  * First-Come First Served CPU Scheduling Algorithm
  * @author Joette Damo
  */
 public class FCFS implements SchedulerInterface {
 
+    /**
+     * Processes for scheduler simulation
+     */
     private List<Process> processes;
+    /**
+     * Total time scheduler is active for all processes
+     */
+    private float totalExecutionTime;
+    /**
+     * True if processes have been executed, false if otherwise
+     */
+    private Boolean processesExecuted;
 
     /**
      * @inheritDoc
@@ -22,7 +37,24 @@ public class FCFS implements SchedulerInterface {
      * @inheritDoc
      */
     @Override
-    public void executeProcesses() {
+    public void executeProcesses(Boolean contextStream) {
+        float startTime = System.currentTimeMillis();
 
+        // YOUR ALGORITHM HERE :)
+
+        float endTime = System.currentTimeMillis();
+        this.totalExecutionTime = endTime - startTime;
+        this.processesExecuted = true;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    @Override
+    public PerformanceMetricGenerator generatePerformanceMetrics() throws Exception {
+        if (!processesExecuted) {
+            throw new Exception("Must complete processes before generating metrics");
+        }
+        return new PerformanceMetricGenerator("First Come First Served", processes, totalExecutionTime);
     }
 }
--- src/main/java/schedulers/SchedulerInterface.java
+++ src/main/java/schedulers/SchedulerInterface.java
@@ -2,6 +2,9 @@ package schedulers;
 
 import java.util.List;
 
+import utilities.PerformanceMetricGenerator;
+import utilities.Process;
+
 /**
  * Interface for simulated CPU schedulers
  * @author Nick Trimmer
@@ -16,6 +19,19 @@ public interface SchedulerInterface {
 
     /**
      * Begins the execution of the scheduler
+     * @param contextStream true if context stream is to be
+     *                      written to a file, false if else
      */
-    void executeProcesses();
+    void executeProcesses(Boolean contextStream);
+
+    /**
+     * Generates scheduler performance metrics for processes given
+     * Metrics: 1. total time needed to complete all processes
+     *          2. CPU utilization
+     *          3. waiting time for each process and average waiting time for all
+     *          4. turnaround time for each process and average turnaround time for all
+     *          5. response time for each process and average response time for all
+     * @throws Exception if process execution has not finished, throw
+     */
+    PerformanceMetricGenerator generatePerformanceMetrics() throws Exception;
 }
